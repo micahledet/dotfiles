3e2602bbd130f3b37d14d56703df2281b215be8c
Micah <micah.ledet@gmail.com>
2024-06-09 20:32:50 -0500

    add nvim lua config

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,57 @@
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    handlers = {
+        function(server_name)
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            require("lspconfig")[server_name].setup({
+                capabilities = capabilities
+            })
+        end,
+    },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local bufnr = args.buf
+        local opts = {buffer = bufnr, remap = false}
+		vim.keymap.set("n","gd",function() vim.lsp.buf.definition() end, opts) --go to definition
+		vim.keymap.set('n','K',function() vim.lsp.buf.hover() end, opts) -- hover
+		vim.keymap.set('n','<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts) --view workspace
+		vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_foat() end, opts) --view diagnostic
+		vim.keymap.set('n','[d',function() vim.diagnostic.goto_next() end, opts)
+		vim.keymap.set('n',']d',function() vim.diagnostic.goto_prev() end, opts)
+		vim.keymap.set('n','<leader>vca', function() vim.lsp.buf.code_action() end, opts) --view code action
+		vim.keymap.set('n','<leader>vrn', function() vim.lsp.buf.rename() end, opts) --rename variables
+		vim.keymap.set('n','<leader>vrr', function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set('n','<leader>h', function() vim.lsp.buf.signature_help() end, opts)
+    end
+})
+
+local lspconfig = require("lspconfig")
+
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
---
+++ nvim/init.lua
@@ -0,0 +1,41 @@
+-- general options
+-- see `:help vim.opt` and `:help option-list`
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+-- set tab spaces to 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.expandtab = true
+
+-- sync clipboard between OS and Neovim
+vim.opt.clipboard = 'unnamedplus'
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- show which line your cursor is on
+-- vim.opt.cursorline = true
+
+-- set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- fast update time
+vim.opt.updatetime = 50
+
+-- color column to 80 characters
+-- vim.opt.colorcolumn = "80"
+
+vim.opt.termguicolors = false
+
+-- require lazy.nvim package manager config file
+require('config.lazy')
